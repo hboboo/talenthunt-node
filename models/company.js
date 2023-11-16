91037ee7ae8b8d5d05a96669e107429aa70c12dc
hb <515870153@qq.com>
2023-11-17 00:02:41 +0800

    新增，公司、岗位模型

---
+++ models/company.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  name: String,
+  financing: {
+    type: String,
+    enum: ['未融资','天使轮','A轮','B轮','C轮','上市']
+  },
+  scale_company: {
+    type: String,
+    enum: ['0-20人','20-99人','100-499人','500-999人','1000人以上']
+  },
+  industry: {
+    type: String,
+    enum: ['互联网', '金融', '教育', '医疗', '电商', '制造业', '媒体', '房地产']
+  },
+  workingHours: {
+    type: {
+      start: String, 
+      end: String
+    }
+  },
+  rest_weekend: {
+    type: String,
+    enum: ['双休','单休','大小周','单休']
+  },
+  overtime: {
+    type: String,
+    enum: ['不加班','偶尔加班','经常加班']
+  },
+  company_address: [String],
+  company_introduction: [String]
+})
+
+module.exports = mongoose.model('Company', schema)
---
+++ models/job.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const shema = new mongoose.Schema({
+  jobname: String,
+  salary: {
+    type: String,
+    enum: ['3k以下','3-5k','5-10k','10-20k','20k以上']
+  },
+  hot: Boolean,
+  is_full_time: Boolean,
+  city: String,
+  district: String,
+  short_company_name: String,
+  job_tag: [String],
+  job_responsibility: [String],
+  job_require: [String],
+  job_experience: {
+    type: String,
+    enum: ['在校生','应届生','1年以内','1-3年','3-5年','5-10年','10年以上']
+  },
+  education: {
+    type: String,
+    enum: ['初中以下', '中专', '高中', '大专', '本科', '硕士', '博士']
+  },
+  recruiter: String
+})
+
+module.exports = mongoose.model('Job', shema)
